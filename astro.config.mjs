3c72159fbde960dedcd89ee01504d212bdc06ea1
DEALUCE <theissakgroup@gmail.com>
2026-04-13 23:31:32 -0700

    fix urls, add faq page, cloudflare headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://rentlaelite.pages.dev',
+  site: 'https://rentlaelite.theissakgroup.workers.dev',
   integrations: [tailwind()],
   output: 'static',
 });
